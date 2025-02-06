fa04533133cd339e6c70373b91cec206378b104d
ArthurBilyk02 <arthurbilyk82@gmail.com>
2025-02-06 18:22:52 +0000

    Added User Hierarchy and Authentication

--- farm-management-backend/middlewares/authMiddleware.js
+++ farm-management-backend/middlewares/authMiddleware.js
@@ -11,7 +11,7 @@ exports.verifyToken = (req, res, next) => {
 
     try {
         const decoded = jwt.verify(token, process.env.JWT_SECRET);
-        req.user = decoded; // Attach user info to request
+        req.user = decoded;
         next();
     } catch (err) {
         res.status(401).json({ error: "Invalid token" });
