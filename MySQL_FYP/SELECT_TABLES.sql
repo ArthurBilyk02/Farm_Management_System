2c975ecabb38ad2f7e6328beb4afa71d1e75d6cf
ArthurBilyk02 <arthurbilyk82@gmail.com>
2025-02-26 18:41:48 +0000

    Farm Management Front-end

--- MySQL_FYP/SELECT_TABLES.sql
+++ MySQL_FYP/SELECT_TABLES.sql
@@ -1,9 +1,18 @@
 USE farm_management;
+
+SELECT * FROM Farm;
 SELECT * FROM Species;
+SELECT * FROM Herd;
 SELECT * FROM Animal;
 SELECT * FROM Feeding_Schedule;
+SELECT * FROM Supplier;
+SELECT * FROM Product_Stock;
 SELECT * FROM Transactions;
+SELECT * FROM Veterinary_Visits;
 SELECT * FROM Medical_History;
+SELECT * FROM Health_Issues;
+SELECT * FROM Environmental_Conditions;
+SELECT * FROM Performance_Metrics;
 SELECT * FROM Users;
-SELECT * FROM Herd;
-SELECT * FROM Farm;
+SELECT * FROM Alert_System;
+SELECT * FROM Feeding_Type
